c86f65c1e58e5b932affdc379523a21ac136fccc
David Martinez <davidmr2431@gmail.com>
2020-10-15 14:44:01 +0200

    AE2-6

---
+++ Martinez_Romaguera_David_AE2/Ejercicio6/js/ae2_6.js
@@ -0,0 +1,6 @@
+let users = ["Pedro","Ricardito","Anacleto","Jesulin","Alberto"];
+
+let blackListed = ["Pedro","Alberto"];
+
+let filt = users.filter(a => !blackListed.includes(a))
+console.log(filtro);
